550c02daa24b267205d528da8644157c206a19aa
DevDaddy <0xkash22@gmail.com>
2023-05-14 17:40:51 -0400

    first commit

---
+++ src/main/java/devdaddy/logbackLogging/LogbackLoggingApplication.java
@@ -0,0 +1,13 @@
+package devdaddy.logbackLogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devdaddy/logbackLogging/LogbackLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package devdaddy.logbackLogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
